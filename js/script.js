6d17e4b966075fe038d508cd533d932468ef9617
perezalexreal <perezalexreal@gmail.com>
2022-01-19 16:06:46 +0100

    Milestone 3 fatta

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
-new Vue ({
+new Vue({
     el: "#app",
     data: {
+        messageToSend: '',
+        currentChat: 'Michele',
         contacts: [
             {
                 name: 'Michele',
@@ -46,6 +48,63 @@ new Vue ({
                     }
                 ],
             },
+            {
+                name: 'Samuele',
+                avatar: '/Users/alexanderjaviercabelloperez/Desktop/Boolean /vue-boolzapp/img/avatar_3.jpg',
+                visible: true,
+                messages: [
+                    {
+                        date: '28/03/2020 10:10:40',
+                        text: 'La Marianna va in campagna',
+                        status: 'received'
+                    },
+                    {
+                        date: '28/03/2020 10:20:10',
+                        text: 'Sicuro di non aver sbagliato chat?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '28/03/2020 16:15:22',
+                        text: 'Ah scusa!',
+                        status: 'received'
+                    }
+                ],
+            },
+            {
+                name: 'Roberto',
+                avatar: '/Users/alexanderjaviercabelloperez/Desktop/Boolean /vue-boolzapp/img/avatar_4.jpg',
+                visible: true,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'Lo sai che ha aperto una nuova pizzeria?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        text: 'Si, ma preferirei andare al cinema',
+                        status: 'received'
+                    }
+                ],
+            },
         ]
+    },
+    methods: {
+        selectChat: function(i){
+            this.currentChat = this.contacts[i].name;
+        },
+        addMessage: function(currentChat) {
+            this.contacts.forEach(element => {
+                if( currentChat == element.name){
+                    element.messages.push({date: new Date().toLocaleString().replace(',', ''), text:  this.messageToSend , status: 'sent'})
+                    this.messageToSend = '';
+                    setTimeout(() => {
+                        element.messages.push({date: new Date().toLocaleString().replace(',', ''), text:  'Ok', status: 'received'})
+                    }, 1000);
+                }
+            });
+        }
     }
 });
+
+
